8768f7cae2c8c8b38fbd8759aad8a17f4d2dc3eb
Gowtham Kumar <g4gowthamkumar@gmail.com>
2023-06-13 21:29:42 +0530

    Delete frame.php

--- frame.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-	<title>frame</title>
-</head>
-<iframe src="click.html" height='200' width='400'></iframe>
-<iframe src="random.php" name="i_frame1" height='200' width='400'></iframe>
-</html>
